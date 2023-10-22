be2808439b32abaf1aa64ab2a2b34c64e213cc35
M Sayeem Ahmed <108409022+9964728107@users.noreply.github.com>
2023-10-22 12:03:26 -0400

    Add files via upload

---
+++ public/js/custom.js
@@ -0,0 +1,11 @@
+// custom.js
+
+// Function to initialize Particle.js with your configuration
+function initializeParticles() {
+    particlesJS.load('particles-js', 'particles.json', function () {
+        console.log('particles.json loaded...');
+    });
+}
+
+// Call the initialization function
+initializeParticles();
